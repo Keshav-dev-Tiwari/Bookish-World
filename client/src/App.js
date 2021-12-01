c9ba2c47146ce0cd31862ed2f8e4b6951f3412f2
Keshav Dev Tiwari <keshav.tiwari_cs19@gla.ac.in>
2021-12-01 15:16:39 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Auth from './components/Auth/Auth';
+
+const App = () => (
+  <BrowserRouter>
+  <Navbar />
+    <Container maxWidth="md">
+    
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
+    </Container>
+  </BrowserRouter>
+);
+
+export default App;
