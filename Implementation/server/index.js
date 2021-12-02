3f15bcaa9b96deb0311ac35ccf36571a025824d7
Keshav Dev Tiwari <keshav.tiwari_cs19@gla.ac.in>
2021-12-02 12:47:57 +0530

    Update index.js

--- Implementation/server/index.js
+++ Implementation/server/index.js
@@ -16,11 +16,9 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://Riya:shaurya20@cluster0.t16fm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://Keshav:Keshav@bookishworld.aagbm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
-
-mongoose.set('useFindAndModify', false);
